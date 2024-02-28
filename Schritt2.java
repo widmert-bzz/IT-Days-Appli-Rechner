448c61bc5d4ba66285b14e8b2989f8db02efb2e2
ia23a-hartwigm <hartwigm@bzz.ch>
2024-02-28 10:57:50 +0100

    schritt2 upgrade done

--- Schritt2.java
+++ Schritt2.java
@@ -1,38 +1,51 @@
 import java.util.Scanner;
 
 public class Schritt2 {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner meineEingabe = new Scanner(System.in);
+        Scanner meineEingabe = new Scanner(System.in);
 
-    System.out.println("Hallo!");
-    System.out.print("Gib deine erste Zahl ein: ");
+        System.out.println("Hallo!");
+        System.out.print("Gib deine erste Zahl ein: ");
 
-    int ersteZahl = meineEingabe.nextInt();
+        int ersteZahl = meineEingabe.nextInt();
 
-    System.out.print("Gib deine zweite Zahl ein: ");
+        System.out.println("Gib einen Operator ein (+, -, *)");
 
-    int zweiteZahl = meineEingabe.nextInt();
+        String operator = meineEingabe.next();
 
+        System.out.print("Gib deine zweite Zahl ein: ");
 
-    float resulat = ersteZahl + zweiteZahl;
+        int zweiteZahl = meineEingabe.nextInt();
 
-    System.out.println(resulat);
-  }
+        switch (operator) {
+            case "+":
+                plus(ersteZahl, zweiteZahl);
+                break;
+            case "-":
+                minus(ersteZahl, zweiteZahl);
+                break;
+            case "*":
+                mal(ersteZahl, zweiteZahl);
+                break;
+            default:
+                System.out.println("Dieser Operator gibt es nicht.");
+        }
+    }
 
-  static void plus(int ersteZahl, int zweiteZahl) {
-    System.out.println(ersteZahl + zweiteZahl);
-  }
+    static void plus(int ersteZahl, int zweiteZahl) {
+        System.out.println(ersteZahl + zweiteZahl);
+    }
 
-  static void minus(int ersteZahl, int zweiteZahl) {
-    System.out.println(ersteZahl - zweiteZahl);
-  }
+    static void minus(int ersteZahl, int zweiteZahl) {
+        System.out.println(ersteZahl - zweiteZahl);
+    }
 
-  static void mal(int ersteZahl, int zweiteZahl) {
-    System.out.println(ersteZahl * zweiteZahl);
-  }
+    static void mal(int ersteZahl, int zweiteZahl) {
+        System.out.println(ersteZahl * zweiteZahl);
+    }
 
-  static void geteilt(int ersteZahl, int zweiteZahl) {
-    System.out.println(ersteZahl / zweiteZahl);
-  }
+    static void geteilt(int ersteZahl, int zweiteZahl) {
+        System.out.println(ersteZahl / zweiteZahl);
+    }
 }
